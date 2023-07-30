d7f7badb8a37910e96acb5a94c0d6b44b2ddbad2
Nirajkavishka <niraj@pramixit.com>
2023-07-30 19:34:19 +0530

    fix: dashboard fix, ui fix

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Grn;
+use App\Models\Inventory;
+use App\Models\SalesNote;
+use App\Models\TransferNote;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -9,6 +13,25 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        return Inertia::render('Dashboard');
+        $product_count = Inventory::select('id')->count();
+        $low_stock_items = Inventory::whereColumn('reorder_point', '>', 'qty')->count();
+        $grn_count = Grn::select('id')->count();
+        $last_gnr = Grn::latest()->first();
+        $tnote_count = TransferNote::select('id')->count();
+        $last_tnote = TransferNote::latest()->first();
+        $snote_count = SalesNote::select('id')->count();
+        $last_snote = SalesNote::latest()->first();
+        return Inertia::render('Dashboard', [
+            'data' => [
+                'product_count' => $product_count,
+                'low_stock_count' => $low_stock_items,
+                'grn_count' => $grn_count,
+                'tnote_count' => $tnote_count,
+                'snote_count' => $snote_count,
+                'last_grn' => ($last_gnr) ? 'Rs '.number_format($last_gnr->total, 2) : 'Rs 0.00',
+                'last_tnote' => ($last_tnote) ? 'Rs '.number_format($last_tnote->total, 2) : 'Rs 0.00',
+                'last_snote' => ($last_snote) ? 'Rs '.number_format($last_snote->total, 2) : 'Rs 0.00',
+            ]
+        ]);
     }
 }
--- resources/js/Components/Sidebar.vue
+++ resources/js/Components/Sidebar.vue
@@ -20,7 +20,7 @@ function closeSideMenu() {
 <template>
   <aside class="z-20 hidden w-64 overflow-y-auto bg-white dark:bg-gray-800 md:block flex-shrink-0">
     <div class="py-4 text-gray-500 dark:text-gray-400">
-      <a class="ml-6 text-lg font-bold text-gray-800 dark:text-gray-200" href="#"> Windmill </a>
+      <a class="ml-6 text-lg font-bold text-gray-800 dark:text-gray-200" href="#"> Gstock </a>
       <ul class="mt-6">
         <template v-for="(item, index) in menu" :key="index">
           <template v-if="item.hasOwnProperty('items') && item.items.length > 0">
